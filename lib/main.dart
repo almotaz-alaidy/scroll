a141e41d23e03c08452a1884322bb8161d65b34a
almotaz alaidy <almotazalaidy36gmale.com>
2022-08-23 18:04:39 +0300

    ubdate

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(App());
-  
-
 }
 
 class App extends StatelessWidget {
@@ -19,7 +17,6 @@ class App extends StatelessWidget {
         drawer: Drawer(),
         body: SingleChildScrollView(
           scrollDirection: Axis.vertical,
-          
           child: Column(
             children: [
               Container(
@@ -41,26 +38,95 @@ class App extends StatelessWidget {
                   ),
                 ),
               ),
-             
-                  Container(
-                    height: 300,
-                    width: 400,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("images/3.webp"),
+              Container(
+                child: Text(
+                  "scroll to left her",
+                  style: TextStyle(
+                      color: Color.fromARGB(255, 110, 110, 109), fontSize: 50),
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      height: 300,
+                      width: 400,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("images/1.png"),
+                        ),
                       ),
                     ),
-                  ),
-                   Container(
-                    height: 300,
-                    width: 400,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("images/4.webp"),
+                    SizedBox(height: 20),
+                    Container(
+                      height: 300,
+                      width: 400,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("images/2.webp"),
+                        ),
                       ),
                     ),
+                    Container(
+                      height: 300,
+                      width: 400,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("images/3.webp"),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 300,
+                      width: 400,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("images/4.webp"),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      height: 300,
+                      width: 400,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("images/5.webp"),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      height: 300,
+                      width: 400,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("images/1.png"),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: 300,
+                width: 400,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("images/3.webp"),
                   ),
-              
+                ),
+              ),
+              Container(
+                height: 300,
+                width: 400,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("images/4.webp"),
+                  ),
+                ),
+              ),
               Divider(
                 height: 5,
               ),
@@ -86,9 +152,7 @@ class App extends StatelessWidget {
             ],
           ),
         ),
-        
       ),
     );
-    
   }
 }
